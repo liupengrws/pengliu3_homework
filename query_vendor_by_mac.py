5081a9d397641afb43ca2dd3cf4efe53240e391d
pengliu3 <pengliu3@cisco.com>
2020-04-30 01:31:27 +0900

    add query_vendor_by_mac.py

---
+++ query_vendor_by_mac.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+"""homework: 
+@author: Peng Liu
+@contact: pengliu3@cisco.com
+@file: query_vendor_by_mac.py
+@time: 2020/04/30 12:00
+@desc: query vendor name by mac address
+"""
+
+import os, sys, time, traceback
+import requests
+
+url = 'https://api.macaddress.io/v1'
+headers = {
+    'Cache-Control': 'no-cache',
+    'Accept': 'application/json',
+    'X-Authentication-Token': 'at_7qbk4lVuq1Lxeaz6v6eYUnUweroka'
+}
+
+def query_vendor(maddr):
+	params = {'search': maddr}
+	res = requests.get(url, headers=headers, params=params)
+	print res.text
+
+if __name__ == '__main__':
+	if (sys.argv) < 2:
+		print 'Please input MAC address.'
+
+	query_vendor(sys.argv[1])
+
